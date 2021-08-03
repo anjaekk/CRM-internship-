8ece8f9e842eb961072b63c919d9c3c800b11969
anjaekk <anjaekk@gmail.com>
2021-08-03 17:20:55 +0900

    Add: user model

--- api/users/models.py
+++ api/users/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
-# Create your models here.
+DEPARTMENT = (
+    (0, '임원'),
+    (1, '경영지원'),
+    (2, '영업지원'),
+    (3, '개발'),
+    (4, '마케팅'),
+)
+
+JOB_TITLE = (
+    (0, '대표이사'),
+    (1, '이사'),
+    (2, '부장'),
+    (3, '차장'),
+    (4, '과장'),
+    (5, '대리'),
+    (6, '주임'),
+    (7, '사원'),
+)
+
+class UserManager(BaseUserManager):
+    def create_user(self, employee_number, name, password=None):
+        if not employee_number:
+            raise ValueError('must have user employee_number')
+        if not name:
+            raise ValueError('must have user name')
+        user = self.model(
+            employee_number = employee_number,
+            name = name
+        )
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    employee_number = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45, unique=True)
+    department = models.SmallIntegerField(choices=DEPARTMENT)
+    job_title = models.SmallIntegerField(choices=JOB_TITLE)
+    image_url = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'employee_number'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        db_table = 'users'
