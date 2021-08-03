35b465d3d56d17672b30e2ee4932a4b4b6eaf459
anjaekk <anjaekk@gmail.com>
2021-08-03 21:17:44 +0900

    Modify: customers app 삭제 및 companies app 생성

---
+++ api/sales/models/sale.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from sales.models.timestamp import TimeStamp
+
+PROGRESS = (
+    (0, ''),
+    (1, ''),
+    (2, ''),
+    (3, ''),
+    (4, ''),
+)
+
+class Sale(TimeStamp):
+    product = 
+    customer =
+    progress = 
