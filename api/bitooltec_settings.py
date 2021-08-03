5eca284762de26d4c8668f447fb1433607e9e80a
anjaekk <anjaekk@gmail.com>
2021-08-03 10:40:12 +0900

    django default settings appended

---
+++ api/bitooltec_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bitooltec',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ian)ux027piw_7cq7x((t!zw4xjt2-%6@xo7!!77t1@on+m&!)'
